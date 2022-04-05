60acb464a7a447ffc76d34e21a75bbb1184d9a46
gemnsh <71697321+gemnsh@users.noreply.github.com>
2022-04-06 08:46:08 +0900

    [Silver V] Title: 집합, Time: 5612 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+import sys
+
+M = int(input())
+S = set()
+
+for _ in range(M):
+    tmp = input().strip().split()
+    if len(tmp) == 1:
+        if tmp[0] == "all":
+            S = set([i for i in range(1, 21)])
+        else:
+            S = set()
+    else:
+        func, x = tmp[0], tmp[1]
+        x = int(x)
+        if func == "add":
+            S.add(x)
+        elif func == "remove":
+            S.discard(x)
+        elif func == "check":
+            print(1 if x in S else 0)
+        elif func == "toggle":
+            if x in S:
+                S.discard(x)
+            else:
+                S.add(x)
